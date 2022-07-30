8b8fd2b0d75ae8d1b23d091bb797a1b45773c17e
Faith Makinde <faithmakinde0@gmail.com>
2022-07-30 18:54:27 +0100

    work in progress

--- main.js
+++ main.js
@@ -3,240 +3,283 @@ let nigeria={
         governor: 'Okezie Ikpeazu',
         'deputy governor':'Ude Oko Chukwu',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Okezie Ikpeazu.jpg"
     },
     adamawa:{
         governor: 'Ahmadu Umaru Fintiri',
         'deputy governor':'Crowther Seth',
         party: 'PDP',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Ahmadu Umaru Fintiri.jpg"
     },
     'akwa-ibom':{
          governor: 'Udom Gabriel Emmanuel',
         'deputy governor':'Moses Ekpo',
          party: 'PDP',
-         date: '2015 - 2023'
+         date: '2015 - 2023',
+         image: "Nigeria state\picture\Udom Gabriel Emmanuel.jpg"
 },
     anambra:{
         governor: 'Charles Chukwuma Soludo',
         'deputy governor':'Onyeka Ibezim',
         party: 'APGA',
-        date: '2022 - 2026'
+        date: '2022 - 2026',
+        image: "Nigeria state\picture\Charles Chukwuma Soludo.jpg"
     },
     bauchi:{
         governor: 'Bala Muhammed',
         'deputy governor':'Baba Tela',
         party: 'PDP',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Bala Muhammed.jpg"
     },
     bayelsa:{
         governor: 'Douye Diri',
         'deputy governor':'Lawrence Ewhrudjakpo',
         party: 'PDP',
-        date: '2020 - 2024'
+        date: '2020 - 2024',
+        image: "Nigeria state\picture\Douye Diri.jpg"
     },
     benue:{
         governor: 'Samuel Ortom',
         'deputy governor':'Benson Abounu',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Samuel Ortom.jpg"
     },
     borno:{
         governor: 'Babagana Umara Zulum',
         'deputy governor':'Umar Usman Kadafur',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Babagana Umara Zulum.jpg"
     },
     'cross river':{
         governor: 'Benedict Ayade',
         'deputy governor':'Ivara Esu',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Benedict Ayade.jpg"
     },
     delta:{
         governor: 'Ifeanyi Okowa',
         'deputy governor':'Kingsley Otuaro',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Ifeanyi Okowa.jpg"
     },
     ebonyi:{
         governor: 'Dave Umahi',
         'deputy governor':'Eric Kelechi Igwe',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Dave Umahi.jpg"
     },
     edo:{
         governor: 'Godwin Obaseki',
         'deputy governor':'Philip Shaibu',
         party: 'PDP',
-        date: '2016 - 2024'
+        date: '2016 - 2024',
+        image: "Nigeria state\picture\Godwin Obaseki.jpg"
     },
     ekiti:{
         governor: 'Kayode Fayemi',
         'deputy governor':'Bisi Egbeyemi',
         party: 'APC',
-        date: '2018 - 2022'
+        date: '2018 - 2022',
+        image: "Nigeria state\picture\Kayode Fayemi.jpg"
     },
     enugu:{
         governor: 'Ifeanyi Ugwuanyi',
         'deputy governor':'Cecilia Ezeilo',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Ifeanyi Ugwuanyi.jpg"
     },
     'fct-abuja':{
         governor: 'Bello Mohammed (Minister of Federal Capital Territory)',
         'deputy governor':'Ramatu Tijani (Minister of State for Federal Capital Territory)',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Bello Mohammed.jpg"
         
     },
     gombe:{
         governor: 'Muhammad Inuwa Yahaya',
         'deputy governor':'Manasseh Daniel Jatau',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Muhammad Inuwa Yahaya.jpg"
     },
     imo:{
         governor: 'Hope Uzodinma',
         'deputy governor':'Placid Njoku',
         party: 'APC',
-        date: '2020 - 2023'
+        date: '2020 - 2023',
+        image: "Nigeria state\picture\Hope Uzodinma.jpg"
     },
     jigawa:{
         governor: 'Mohammed Badaru Abubakar',
         'deputy governor':'Umar Namadi',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Mohammed Badaru Abubakar.jpg"
     },
     kaduna:{
         governor: 'Nasir Ahmad el-Rufai',
         'deputy governor':'Hadiza Sabuwa Balarabe',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Nasir Ahmad el-Rufai.jpg"
     },
     kano:{
         governor: 'Abdullahi Umar Ganduje',
         'deputy governor':'Nasir Yusuf Gawuna',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Abdullahi Umar Ganduje.jpg"
     },
     katsina:{
         governor: 'Aminu Bello Masari',
         'deputy governor':'Mannir Yakubu',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Aminu Bello Masari.jpg"
     },
     kebbi:{
         governor: 'Abubakar Atiku Bagudu',
         'deputy governor':'Samaila Yombe Dabai',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Abubakar Atiku Bagudu.jpg"
     },
     kogi:{
         governor: 'Yahaya Bello',
         'deputy governor':'Edward Onoja',
         party: 'APC',
-        date: '2016 - 2024'
+        date: '2016 - 2024',
+        image: "Nigeria state\picture\Yahaya Bello.jpg"
     },
     kwara:{
         governor: 'AbdulRahman AbdulRasaq',
         'deputy governor':'Edward Onoja',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\AbdulRahman AbdulRasaq.jpg"
     },
     lagos:{
         governor: 'Babajide Sanwo-Olu',
         'deputy governor':'Femi Hamzat',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Babajide Sanwo-Olu.jpg"
     },
     nasarawa:{
         governor: 'Abdullahi Sule',
         'deputy governor':'Emmanuel Agbadu Akabe',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Abdullahi Sule.jpg"
     },
     niger:{
         governor: 'Abubakar Sani Bello',
         'deputy governor':'Ahmed Muhammad Ketso',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Abubakar Sani Bello.jpg"
     },
     ogun:{
         governor: 'Dapo Abiodun',
         'deputy governor':'Noimot Salako-Oyedele',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Dapo Abiodun.jpg"
     },
     ondo:{
         governor: 'Oluwarotimi Odunayo Akeredolu',
         'deputy governor':'Lucky Aiyedatiwa',
         party: 'APC',
-        date: '2017 - 2025'
+        date: '2017 - 2025',
+        image: "Nigeria state\picture\Oluwarotimi Odunayo Akeredolu.jpg"
     },
     osun:{
         governor: 'Adegboyega Oyetola',
         'deputy governor':'Gboyega Alabi',
         party: 'APC',
-        date: '2018 - 2022'
+        date: '2018 - 2022',
+        image: "Nigeria state\picture\Adegboyega Oyetola.jpg"
     },
     oyo:{
         governor: 'Oluwaseyi Makinde',
         'deputy governor':'Rauf Aderemi Olaniyan',
         party: 'PDP',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Oluwaseyi Makinde.jpg"
     },
     plateau:{
         governor: 'Simon Lalong',
         'deputy governor':'Sonni Gwanle Tyoden',
         party: 'APC',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Simon Lalong.jpg"
     },
     rivers:{
         governor: 'Ezenwo Nyesom Wike',
         'deputy governor':'Ipalibo Banigo',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Ezenwo Nyesom Wike.jpg"
     },
     sokoto:{
         governor: 'Aminu Waziri Tambuwal',
         'deputy governor':'Alhaji Mannir Dan\'Iya',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Aminu Waziri Tambuwal.jpg"
     },
     taraba:{
         governor: 'Darius Ishaku',
         'deputy governor':'Haruna Manu',
         party: 'PDP',
-        date: '2015 - 2023'
+        date: '2015 - 2023',
+        image: "Nigeria state\picture\Darius Ishaku.jpg"
     },
     yobe:{
         governor: 'Mai Mala Buni',
         'deputy governor':'Idi Barade Gubana',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Mai Mala Buni.jpg"
     },
     zamfara:{
         governor: 'Bello Matawalle',
         'deputy governor':'Hassan Muhammed Gusau',
         party: 'APC',
-        date: '2019 - 2023'
+        date: '2019 - 2023',
+        image: "Nigeria state\picture\Bello Matawalle.jpg"
     }
 }
 function nigeriaState(){
+    let governorList = document.querySelector(".governor_list")
     let question = prompt("Please input any state within Nigeria.");
     let state = question.toLowerCase();
+
+    governorList.innerHTML = `<img src="Nigeria state\picture\Abdullahi Sule.jpg" alt="governor image">`
+
     if(nigeria[state] !== undefined){
         let governorName = nigeria[state]["governor"];
         let deputyName = nigeria[state]["deputy governor"];
         let partyName = nigeria[state]["party"];
         let yearsInOffice = nigeria[state]["date"];
+        let img = nigeria[state]["image"]
     alert(`
     The governor of ${state} state is ${governorName}.
     The deputy governor of ${state} state is ${deputyName}.
     The party name is ${partyName}.
     The date in office is ${yearsInOffice}.`)
     }
+
     else{
         alert(`Not found.`)
     }
