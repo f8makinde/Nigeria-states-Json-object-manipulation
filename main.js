63ef9f2b986be1b593ebfdb3315b432c969bfc87
Faith Makinde <faithmakinde0@gmail.com>
2022-07-30 16:46:26 +0100

    work completed

---
+++ main.js
@@ -0,0 +1,244 @@
+let nigeria={
+    abia: {
+        governor: 'Okezie Ikpeazu',
+        'deputy governor':'Ude Oko Chukwu',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    adamawa:{
+        governor: 'Ahmadu Umaru Fintiri',
+        'deputy governor':'Crowther Seth',
+        party: 'PDP',
+        date: '2019 - 2023'
+    },
+    'akwa-ibom':{
+         governor: 'Udom Gabriel Emmanuel',
+        'deputy governor':'Moses Ekpo',
+         party: 'PDP',
+         date: '2015 - 2023'
+},
+    anambra:{
+        governor: 'Charles Chukwuma Soludo',
+        'deputy governor':'Onyeka Ibezim',
+        party: 'APGA',
+        date: '2022 - 2026'
+    },
+    bauchi:{
+        governor: 'Bala Muhammed',
+        'deputy governor':'Baba Tela',
+        party: 'PDP',
+        date: '2019 - 2023'
+    },
+    bayelsa:{
+        governor: 'Douye Diri',
+        'deputy governor':'Lawrence Ewhrudjakpo',
+        party: 'PDP',
+        date: '2020 - 2024'
+    },
+    benue:{
+        governor: 'Samuel Ortom',
+        'deputy governor':'Benson Abounu',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    borno:{
+        governor: 'Babagana Umara Zulum',
+        'deputy governor':'Umar Usman Kadafur',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    'cross river':{
+        governor: 'Benedict Ayade',
+        'deputy governor':'Ivara Esu',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    delta:{
+        governor: 'Ifeanyi Okowa',
+        'deputy governor':'Kingsley Otuaro',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    ebonyi:{
+        governor: 'Dave Umahi',
+        'deputy governor':'Eric Kelechi Igwe',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    edo:{
+        governor: 'Godwin Obaseki',
+        'deputy governor':'Philip Shaibu',
+        party: 'PDP',
+        date: '2016 - 2024'
+    },
+    ekiti:{
+        governor: 'Kayode Fayemi',
+        'deputy governor':'Bisi Egbeyemi',
+        party: 'APC',
+        date: '2018 - 2022'
+    },
+    enugu:{
+        governor: 'Ifeanyi Ugwuanyi',
+        'deputy governor':'Cecilia Ezeilo',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    'fct-abuja':{
+        governor: 'Bello Mohammed (Minister of Federal Capital Territory)',
+        'deputy governor':'Ramatu Tijani (Minister of State for Federal Capital Territory)',
+        party: 'APC',
+        date: '2015 - 2023'
+        
+    },
+    gombe:{
+        governor: 'Muhammad Inuwa Yahaya',
+        'deputy governor':'Manasseh Daniel Jatau',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    imo:{
+        governor: 'Hope Uzodinma',
+        'deputy governor':'Placid Njoku',
+        party: 'APC',
+        date: '2020 - 2023'
+    },
+    jigawa:{
+        governor: 'Mohammed Badaru Abubakar',
+        'deputy governor':'Umar Namadi',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    kaduna:{
+        governor: 'Nasir Ahmad el-Rufai',
+        'deputy governor':'Hadiza Sabuwa Balarabe',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    kano:{
+        governor: 'Abdullahi Umar Ganduje',
+        'deputy governor':'Nasir Yusuf Gawuna',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    katsina:{
+        governor: 'Aminu Bello Masari',
+        'deputy governor':'Mannir Yakubu',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    kebbi:{
+        governor: 'Abubakar Atiku Bagudu',
+        'deputy governor':'Samaila Yombe Dabai',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    kogi:{
+        governor: 'Yahaya Bello',
+        'deputy governor':'Edward Onoja',
+        party: 'APC',
+        date: '2016 - 2024'
+    },
+    kwara:{
+        governor: 'AbdulRahman AbdulRasaq',
+        'deputy governor':'Edward Onoja',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    lagos:{
+        governor: 'Babajide Sanwo-Olu',
+        'deputy governor':'Femi Hamzat',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    nasarawa:{
+        governor: 'Abdullahi Sule',
+        'deputy governor':'Emmanuel Agbadu Akabe',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    niger:{
+        governor: 'Abubakar Sani Bello',
+        'deputy governor':'Ahmed Muhammad Ketso',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    ogun:{
+        governor: 'Dapo Abiodun',
+        'deputy governor':'Noimot Salako-Oyedele',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    ondo:{
+        governor: 'Oluwarotimi Odunayo Akeredolu',
+        'deputy governor':'Lucky Aiyedatiwa',
+        party: 'APC',
+        date: '2017 - 2025'
+    },
+    osun:{
+        governor: 'Adegboyega Oyetola',
+        'deputy governor':'Gboyega Alabi',
+        party: 'APC',
+        date: '2018 - 2022'
+    },
+    oyo:{
+        governor: 'Oluwaseyi Makinde',
+        'deputy governor':'Rauf Aderemi Olaniyan',
+        party: 'PDP',
+        date: '2019 - 2023'
+    },
+    plateau:{
+        governor: 'Simon Lalong',
+        'deputy governor':'Sonni Gwanle Tyoden',
+        party: 'APC',
+        date: '2015 - 2023'
+    },
+    rivers:{
+        governor: 'Ezenwo Nyesom Wike',
+        'deputy governor':'Ipalibo Banigo',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    sokoto:{
+        governor: 'Aminu Waziri Tambuwal',
+        'deputy governor':'Alhaji Mannir Dan\'Iya',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    taraba:{
+        governor: 'Darius Ishaku',
+        'deputy governor':'Haruna Manu',
+        party: 'PDP',
+        date: '2015 - 2023'
+    },
+    yobe:{
+        governor: 'Mai Mala Buni',
+        'deputy governor':'Idi Barade Gubana',
+        party: 'APC',
+        date: '2019 - 2023'
+    },
+    zamfara:{
+        governor: 'Bello Matawalle',
+        'deputy governor':'Hassan Muhammed Gusau',
+        party: 'APC',
+        date: '2019 - 2023'
+    }
+}
+function nigeriaState(){
+    let question = prompt("Please input any state within Nigeria.");
+    let state = question.toLowerCase();
+    if(nigeria[state] !== undefined){
+        let governorName = nigeria[state]["governor"];
+        let deputyName = nigeria[state]["deputy governor"];
+        let partyName = nigeria[state]["party"];
+        let yearsInOffice = nigeria[state]["date"];
+    alert(`
+    The governor of ${state} state is ${governorName}.
+    The deputy governor of ${state} state is ${deputyName}.
+    The party name is ${partyName}.
+    The date in office is ${yearsInOffice}.`)
+    }
+    else{
+        alert(`Not found.`)
+    }
+}
+nigeriaState()
